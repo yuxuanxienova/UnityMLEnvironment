2e66482fa315b2d533d74b870d6727dd7f9637dd
YuxuanXie <yuxuangmxie@gmail.com>
2024-09-24 20:14:00 +0200

    add trainner, improve server, automate buffer resize

--- Assets/PythonCode/NetworkServer/net/NetManager.py
+++ Assets/PythonCode/NetworkServer/net/NetManager.py
@@ -4,151 +4,194 @@ sys.path.append(os.path.join(os.path.dirname(__file__) , '..'))
 import socket
 import select
 import time
-from typing import List, Dict
+from typing import List, Dict, Callable
 from net.ClientState import ClientState
 from net.ByteArray import ByteArray
 from net.MsgBase import MsgBase
 from proto.Messages import MsgPing, MsgPong , MsgTest
-class EventHandler:
-    @staticmethod
-    def on_disconnect():
+# Define listener types
+EventHandler = Callable[[str], None]
+MsgHandler = Callable[[MsgBase], None]
+class NetManagerServer:
+    def __init__(self) -> None: 
+        # Listening socket
+        self.listen_fd = None
+
+        # Client sockets and states
+        self.clients:Dict[socket.socket,ClientState] = {}
+
+        # Select read list
+        self.check_read = []
+
+        # Ping interval
+        self.ping_interval = 30
+        
+        # Event handling
+        self.event_handlers:Dict[str,EventHandler] ={}
+        self.message_handlers:Dict[str,MsgHandler] ={} 
+        
+        self.add_event_handler("on_disconnect",self.on_disconnect)
+        self.add_event_handler("on_timer",self.on_timer)
+        self.add_event_handler("check_ping",self.check_ping)
+        
+        self.add_msg_handler("MsgPing",self.handle_msg_ping)
+
+#---------------------------------监听结构------------------------------------------
+    #------------------------------1. 网络事件监听----------------------------------------
+    # Add event listener
+    def add_event_handler(self, event_name: str, handler: EventHandler):
+        if event_name not in self.event_handlers:
+            self.event_handlers[event_name] = []
+        self.event_handlers[event_name].append(handler)
+    # Remove event listener
+    def remove_event_handler(self, event_name: str, handler: EventHandler):
+        if event_name in self.event_handlers:
+            self.event_handlers[event_name].remove(handler)
+            if not self.event_handlers[event_name]:
+                del self.event_handlers[event_name]
+    # Fire event
+    def fire_event(self, event_name: str, err: str):
+        if event_name in self.event_handlers:
+            for handler in self.event_handlers[event_name]:
+                handler(err)
+
+    def on_disconnect(self,err:str):
         print("Close")
 
-    @staticmethod
-    def on_timer():
-        EventHandler.check_ping()
+
+    def on_timer(self,err:str):
+        self.check_ping(err)
 
     # Ping check
-    @staticmethod
-    def check_ping():
+    def check_ping(self,err:str):
         # Current timestamp
-        time_now = NetManagerServer.get_time_stamp()
+        time_now = self.get_time_stamp()
 
         # Iterate and remove
-        for s in list(NetManagerServer.clients.values()):
-            if time_now - s.last_ping_time > NetManagerServer.ping_interval * 4:
+        for s in list(self.clients.values()):
+            if time_now - s.last_ping_time > self.ping_interval * 4:
                 print(f"Ping Close {s.socket.getpeername()}")
-                NetManagerServer.close(s)
+                self.close(s)
                 # Since 'close' removes the client from the list,
                 # we break out of the loop to avoid iteration errors
                 break
-class MsgHandler:
-    @staticmethod
-    def MsgPing(client:ClientState, msg_base):
+    #-------------------------------2. 网络消息监听---------------------------------------------
+    # Add message listener
+    def add_msg_handler(self, msg_name: str, handler: MsgHandler):
+        if msg_name not in self.message_handlers:
+            self.message_handlers[msg_name] = []
+        self.message_handlers[msg_name].append(handler)
+    # Remove message listener
+    def remove_msg_handler(self, msg_name: str, handler: MsgHandler):
+        if msg_name in self.message_handlers:
+            self.message_handlers[msg_name].remove(handler)
+            if not self.message_handlers[msg_name]:
+                del self.message_handlers[msg_name]
+    # Fire message
+    def fire_msg(self, msg_name: str, client_state:ClientState ,msg_base: MsgBase):
+        if msg_name in self.message_handlers:
+            for handler in self.message_handlers[msg_name]:
+                handler(client_state , msg_base)
+                
+    def handle_msg_ping(self, client:ClientState, msg_base:MsgBase):
         print("MsgPing")
-        client.last_ping_time = NetManagerServer.get_time_stamp()
+        client.last_ping_time = self.get_time_stamp()
         msg_pong = MsgPong()
-        NetManagerServer.send(client, msg_pong)
+        self.send(client, msg_pong)
         
-    @staticmethod
-    def MsgTest(client:ClientState, msg_base):
-        print("[Msg Received][client:{0}]MsgTest".format(client.client_address) + msg_base.str + str(msg_base.num) )
-        msg_test = MsgTest()
-        msg_test.str = "Hi from server"
-        msg_test.num = 123
-        NetManagerServer.send(client, msg_test)
-        
-    # @staticmethod
-    # def TransitionMsg(client:ClientState, msg_base):
-    #     # print("[Msg Received][client:{0}]TransitionMsg".format(client.client_address) + str(msg_base.state) + str(msg_base.action) + str(msg_base.reward) + str(msg_base.next_state) + str(msg_base.trancated_flag))
-
-
-class NetManagerServer:
-    # Listening socket
-    listen_fd = None
-
-    # Client sockets and states
-    clients:Dict[socket.socket,ClientState] = {}
-
-    # Select read list
-    check_read = []
-
-    # Ping interval
-    ping_interval = 30
-
-    @staticmethod
-    def start_loop(listen_port):
+    #-------------------------------------------------------------------------------------
+    def initialize(self,listen_port):
         # Create socket
-        NetManagerServer.listen_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        NetManagerServer.listen_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         # Bind
-        NetManagerServer.listen_fd.bind(('0.0.0.0', listen_port))
+        self.listen_fd.bind(('0.0.0.0', listen_port))
 
         # Listen
-        NetManagerServer.listen_fd.listen()
+        self.listen_fd.listen()
         print("Server started successfully")
+    def update(self):
+        self.reset_check_read()
 
-        # Main loop
-        while True:
-            NetManagerServer.reset_check_read()
+        # Use select to wait for sockets to be ready
+        read_sockets, _, _ = select.select(self.check_read, [], [], 1)
 
-            # Use select to wait for sockets to be ready
-            read_sockets, _, _ = select.select(NetManagerServer.check_read, [], [], 1)
+        for s in read_sockets:
+            if s == self.listen_fd:
+                self.read_listen_fd()
+            else:
+                self.read_client_fd(s)
 
-            for s in read_sockets:
-                if s == NetManagerServer.listen_fd:
-                    NetManagerServer.read_listen_fd()
-                else:
-                    NetManagerServer.read_client_fd(s)
+        # Handle timeouts
+        self.timer()
 
-            # Handle timeouts
-            NetManagerServer.timer()
+    
+    def reset_check_read(self):
+        self.check_read = [self.listen_fd]
+        for state in self.clients.values():
+            self.check_read.append(state.socket)
 
-    @staticmethod
-    def reset_check_read():
-        NetManagerServer.check_read = [NetManagerServer.listen_fd]
-        for state in NetManagerServer.clients.values():
-            NetManagerServer.check_read.append(state.socket)
-
-    @staticmethod
-    def read_listen_fd():
+    
+    def read_listen_fd(self):
         try:
-            client_fd, addr = NetManagerServer.listen_fd.accept()
+            client_fd, addr = self.listen_fd.accept()
             print(f"Accepted connection from {addr}")
             state = ClientState(client_socket=client_fd)
-            state.last_ping_time = NetManagerServer.get_time_stamp()
-            NetManagerServer.clients[client_fd] = state
+            state.last_ping_time = self.get_time_stamp()
+            self.clients[client_fd] = state
         except socket.error as ex:
             print(f"Accept failed: {ex}")
 
-    @staticmethod
-    def read_client_fd(client_fd):
-        state:ClientState = NetManagerServer.clients.get(client_fd)
-        read_buffer = state.read_buffer
-
+    
+    def read_client_fd(self,client_fd):
+        client_state:ClientState = self.clients.get(client_fd)
+        client_read_buffer = client_state.read_buffer
+        
+        #缓冲区不够，清除，若依旧还不够，只能返回
+        #缓冲区长度只有1024， 单条协议超过缓冲区长度时会发生错误，根据需要调整长度
+        print("[INFO][Net Server][Client:{0}]Client Read Buffer Remain:{1}".format(client_state.client_address,client_read_buffer.remain))
+        if client_read_buffer.remain <= 0:
+            client_read_buffer.resize(client_read_buffer.capacity + 1024)
+            self.on_receive_data(client_state)
+            client_read_buffer.check_and_move_bytes()
+            
+        if client_read_buffer.remain <= 0:
+            print("[ERROR][Net Server]Receive Fail, mabe msg length > buff capacity")
+            self.close(client_state)
+            return
        
         try:
             # Receive data
-            data = client_fd.recv(read_buffer.remain)
-            if not data:
-                print(f"Client disconnected: {client_fd.getpeername()}")
-                NetManagerServer.close(state)
-                return
+            data = client_fd.recv(client_read_buffer.remain)
+            # if not data:
+            #     print(f"Client disconnected: {client_fd.getpeername()}")
+            #     self.close(client_state)
+            #     return
             # Write data to buffer
-            read_buffer.write(data, 0, len(data))
+            client_read_buffer.write(data, 0, len(data))
 
             # Process received data
-            NetManagerServer.on_receive_data(state)
+            self.on_receive_data(client_state)
 
             # Move buffer if necessary
-            read_buffer.check_and_move_bytes()
+            client_read_buffer.check_and_move_bytes()
 
         except socket.error as ex:
-            print(f"Receive socket exception: {ex}")
-            NetManagerServer.close(state)
+            print(f"[ERROR][Net Server]Receive socket exception: {ex}")
+            self.close(client_state)
 
-    @staticmethod
-    def close(state):
+    
+    def close(self,state):
         # Event handling
-        EventHandler.on_disconnect(state)
+        self.fire_event("on_disconnect", "")
 
         # Close socket and remove from clients
         state.socket.close()
-        del NetManagerServer.clients[state.socket]
+        del self.clients[state.socket]
 
-    @staticmethod
-    def on_receive_data(state):
+    
+    def on_receive_data(self,state):
         read_buffer = state.read_buffer
 
         while True:
@@ -169,7 +212,7 @@ class NetManagerServer:
             proto_name, name_count = MsgBase.decode_name(read_buffer.bytes, read_buffer.read_idx)
             if not proto_name:
                 print("Failed to decode message name")
-                NetManagerServer.close(state)
+                self.close(state)
                 return
 
             read_buffer.read_idx += name_count
@@ -180,23 +223,23 @@ class NetManagerServer:
             read_buffer.read_idx += body_count
 
             # Handle message
-            print(f"Received message: {proto_name}")
-            handler = getattr(MsgHandler, proto_name, None)
-            if handler:
-                handler(state, msg_base)
+            # print(f"Received message: {proto_name}")
+
+            if proto_name in self.message_handlers:
+                self.fire_msg(proto_name, state , msg_base)
             else:
                 print(f"No handler for message: {proto_name}")
 
             # Move buffer if necessary
             read_buffer.check_and_move_bytes()
 
-    @staticmethod
-    def timer():
+    
+    def timer(self):
         # Event handling
-        EventHandler.on_timer()
+        self.fire_event("on_timer", "")
 
-    @staticmethod
-    def send(cs, msg):
+    
+    def send(self,cs, msg):
         # Check state
         if cs is None or cs.socket.fileno() == -1:
             return
@@ -221,8 +264,20 @@ class NetManagerServer:
             cs.socket.sendall(send_bytes)
         except socket.error as ex:
             print(f"Socket closed on sendall: {ex}")
-            NetManagerServer.close(cs)
+            self.close(cs)
 
     @staticmethod
     def get_time_stamp():
         return int(time.time())
+if __name__ == "__main__":
+    SERVER_PORT = 65432;
+    server = NetManagerServer()
+    
+    def handle_msg_test(msg_base:MsgBase):
+        print("MsgTest"+msg_base.str + str(msg_base.num))
+    server.add_msg_handler("MsgTest",handle_msg_test)
+    
+    server.initialize(SERVER_PORT)
+    while True:
+        server.update()
+        time.sleep(0.01)
--- Assets/PythonCode/Trainner/run_trainner.py
+++ Assets/PythonCode/Trainner/run_trainner.py
@@ -1,10 +1,10 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from NetworkServer.net.NetManager import NetManagerServer, EventHandler, MsgHandler
 from NetworkServer.net.ClientState import ClientState
 from NetworkServer.proto.Messages import MsgPing, MsgPong, MsgTest, TransitionMsg, SampleActionRequestMsg, SampleActionResponseMsg
-from Trainner.PythonClient.net.NetManager import NetManagerClient, NetEvent
+from NetworkServer.net.MsgBase import MsgBase
+from NetworkServer.net.NetManager import NetManagerServer
 import numpy as np
 import torch
 import PIL
@@ -25,11 +25,12 @@ def clean_log_dir(log_dir):
 class Trainner:
     def __init__(self) -> None:
         #Parameters
-        self.state_dim = 73#73
-        self.action_dim = 16#16
+        self.task_name = "3D_Ball"
+        self.state_dim = 8
+        self.action_dim = 2
         self.trainTime=1000
-        self.log_dir= "./catkin_ws/src/RL_trainner_pkg/logs"
-        self.save_dir = "./catkin_ws/src/RL_trainner_pkg/models/"
+        self.log_dir= os.path.join(os.path.dirname(__file__), 'logs/' + self.task_name + '/')
+        self.save_dir = os.path.join(os.path.dirname(__file__), 'saved_model/' + self.task_name + '/')
         self.save_interval = 1000
         self.log_interval = 20
         # initialize components
@@ -41,32 +42,36 @@ class Trainner:
         self.num_update=0
         self.timePassed=0
         
-        #Initialize NetManagerClient
-        self.client = NetManagerClient()
-        def on_connect_success(err):
-            print("Connected successfully!")
-        def on_connect_fail(err):
-            print(f"Failed to connect: {err}")
-        def on_close(err):
-            print("Connection closed.")
-        def on_pong(msg):
-            print("Received PONG")
-        def TransitionMsgHandler(msg_base):
+        SERVER_PORT = 65432;
+        self.server = NetManagerServer()
+        
+        def TransitionMsgHandler(client_state:ClientState , msg_base:MsgBase):
+            print("[Msg Received][client:{0}]TransitionMsg".format(client_state.client_address) + str(msg_base.state) + str(msg_base.action) + str(msg_base.reward) + str(msg_base.next_state) + str(msg_base.trancated_flag))
             state = np.array(msg_base.state)
             action = np.array(msg_base.action)
             reward = np.array(msg_base.reward)
             next_state = np.array(msg_base.next_state)
             trancated_flag = msg_base.trancated_flag
             transition = (state,action,reward,next_state)
-            print("[INFO][TransitionMsgHandler]transition=(0)".format(transition))
             if(state.shape[0]==self.state_dim and action.shape[0]==self.action_dim and next_state.shape[0]==self.state_dim):
                 self.agent.memory.put(transition)
-                        
-        self.client.add_event_listener(NetEvent.CONNECT_SUCC, on_connect_success)
-        self.client.add_event_listener(NetEvent.CONNECT_FAIL, on_connect_fail)
-        self.client.add_event_listener(NetEvent.CLOSE, on_close)
-        self.client.add_msg_listener("MsgPong", on_pong)
-        self.cloent.add_msg_listener("TransitionMsg", TransitionMsgHandler)
+        def SampleActionRequestMsgHandler(client:ClientState, msg_base):
+            print("[Msg Received][client:{0}]SampleActionMsg".format(client.client_address) + str(msg_base.state))
+            state = np.array(msg_base.state)
+            action = self.agent.get_action(state,train=False)
+            response = SampleActionResponseMsg()
+            response.action = action.tolist()
+            print("[Msg Send][client:{0}]SampleActionResponseMsg".format(client.client_address) + str(response.action))
+            self.server.send(client, response)  
+        def EpisodicRewardMsgHandler(client:ClientState, msg_base):
+            reward = np.array(msg_base.data)
+            self.summary_writer.add_scalar("episodic_reward", reward, self.timePassed)    
+            
+        self.server.add_msg_handler("TransitionMsg",TransitionMsgHandler)
+        self.server.add_msg_handler("SampleActionRequestMsg",SampleActionRequestMsgHandler)
+        # self.server.add_msg_handler("EpisodicRewardMsg",EpisodicRewardMsgHandler)
+        
+        self.server.initialize(SERVER_PORT)
 
     def run(self):
         
@@ -80,7 +85,8 @@ class Trainner:
             current_time = time.time()
             
             #update client
-            self.client.update()
+            self.server.update()
+            
             # Check if it's time to run updateNetwork
             if current_time - last_network_update >= network_interval:
                 self.updateNetwork(None)
